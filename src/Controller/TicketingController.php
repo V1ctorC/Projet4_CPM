422f5708c4c2c64c28f82df8ca47ae5e7fbe44e9
VictorC <victor.du.77@hotmail.fr>
2018-12-06 16:10:18 +0100

    add form -> UserType + test with contactInformation

--- src/Controller/TicketingController.php
+++ src/Controller/TicketingController.php
@@ -2,6 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -19,9 +25,23 @@ class TicketingController extends AbstractController
     /**
      * @Route ("/contact_information")
      */
-    public function contactInformation()
+    public function contactInformation(Request $request)
     {
-        return $this->render('Ticketing/contactInformation.html.twig');
+        $user = new User();
+        $em = $this->getDoctrine()->getManager();
+
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $user = $form->getData();
+        }
+
+        return $this->render('Ticketing/contactInformation.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 
     /**
