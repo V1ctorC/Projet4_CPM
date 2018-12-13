533b770c1eca307e00e7370247391424643d1aca
VictorC <victor.du.77@hotmail.fr>
2018-12-13 10:38:46 +0100

    Add class -> birthdate to enable datepicker + add style sum -> summary.html.twig

--- src/Form/InformationType.php
+++ src/Form/InformationType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Information;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,9 +14,11 @@ class InformationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('birthdate')
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('birthdate', TextType::class, array(
+                'attr' => array('class' => 'information_birthdate')
+            ))
             ->add('country', CountryType::class, ["preferred_choices" => array('FR')])
             ->add('reducedprice')
         ;
