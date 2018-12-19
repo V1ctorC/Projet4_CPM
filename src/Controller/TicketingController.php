c54cafbf55fbabcf7a7a9bc718b6a4db372c902f
VictorC <victor.du.77@hotmail.fr>
2018-12-19 14:42:59 +0100

    Add regex to check date -> contactInformation.html.twig

--- src/Controller/TicketingController.php
+++ src/Controller/TicketingController.php
@@ -80,6 +80,14 @@ class TicketingController extends AbstractController
         $form = $this->createForm(CollectionType::class, $user, ['entry_type' => InformationType::class]);
         $form->handleRequest($request);
 
+        for ($i = 0; $i<$quantity; $i++)
+        {
+            if (($user[$i]->getBirthdate() != null) && (!preg_match('/[0-3]\d\/[0-1]\d\/[1-2](0|9)\d\d/', $user[$i]->getBirthdate())))
+            {
+                return $this->redirectToRoute('errorDate');
+            }
+        }
+
         if ($form->isSubmitted() && $form->isValid())
         {
 
