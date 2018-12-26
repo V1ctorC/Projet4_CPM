7876054d60002e8b565d572464fa4794be5162af
VictorC <victor.du.77@hotmail.fr>
2018-12-26 12:27:18 +0100

    Add TicketingControllerTest + BookingTypeTest

---
+++ tests/Controller/TicketingControllerTest.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VictorChevalier
+ * Date: 2018-12-26
+ * Time: 12:15
+ */
+
+namespace App\Tests\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class TicketingControllerTest extends WebTestCase
+{
+    /**
+     * @test
+     */
+    public function showHomepage()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    /**
+     * @test
+     */
+    public function contentHomepage()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/');
+
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Billeterie du musée du Louvre")')
+        ->count());
+    }
+}
---
+++ tests/Form/BookingTypeTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: VictorChevalier
+ * Date: 2018-12-26
+ * Time: 10:26
+ */
+
+namespace App\Tests\Form;
+
+
+use App\Entity\Booking;
+use App\Form\BookingType;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class InformationTypeTest extends TypeTestCase
+{
+    /**
+     * @test
+     */
+    public function testSubmitValidData()
+    {
+
+        $formData = array(
+            'bookingNumber' => '1234567890',
+            'bookingDay' => '2018-12-26',
+            'type' => 'W',
+            'paid' => true,
+            'quantity' => 1,
+            'mail' => 'test@test.com'
+        );
+
+        $objectToCompare = new Booking();
+
+        $form = $this->factory->create(BookingType::class, $objectToCompare);
+
+        $object = new Booking();
+        $object->setBookingnumber('1234567890');
+        $object->setBookingday(new \DateTime('today'));
+        $object->setType('W');
+        $object->setPaid(true);
+        $object->setQuantity(1);
+        $object->setMail('test@test.com');
+
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+
+        $this->assertEquals($object, $objectToCompare);
+
+        $view = $form->createView();
+        $children = $view->children;
+
+        foreach (array_keys($formData) as $key)
+        {
+            $this->assertArrayHasKey($key, $children);
+        }
+    }
+}
